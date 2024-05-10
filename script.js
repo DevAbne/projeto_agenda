e4d33bfc6e65f16225c9b15a7a4c73e68225e374
costaabne@gmail.com <Abne.Costa>
2024-05-10 20:53:16 -0300

    alterando type do input telefone

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 document.getElementById("formContato").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita o recarregamento da página ao submeter o formulário
-    // Obter os valores dos campos do formulário
+    event.preventDefault(); 
     var nome = document.getElementById("nome").value;
     var telefone = document.getElementById("telefone").value;
-    // Limpar os campos do formulário
     document.getElementById("nome").value = "";
     document.getElementById("telefone").value = "";
-    // Adicionar uma nova linha à tabela com os valores do formulário
     var tabela = document.getElementById("tabela");
     var novaLinha = tabela.insertRow(-1); // Insere uma nova linha no final da tabela
     var colunaNome = novaLinha.insertCell(0); // Insere uma célula na primeira coluna da nova linha
